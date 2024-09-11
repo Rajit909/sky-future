61a4c69d58d8a5fd6d3675572424f4c978420468
Rajit Kumar <94344369+Rajit909@users.noreply.github.com>
2024-09-11 16:43:13 +0530

    Update NavBar.jsx

--- src/app/(router)/_components/Navbar/NavBar.jsx
+++ src/app/(router)/_components/Navbar/NavBar.jsx
@@ -135,7 +135,7 @@ const NavBar = () => {
               </Dialog>
               </div>
 
-         {/*  {isLoaded && user ? (
+         {isLoaded && user ? (
             <div className="mt-1 ">
               <UserButton afterSignOutUrl="/"  />
             </div>
@@ -145,7 +145,7 @@ const NavBar = () => {
                 Get started
               </Button>
             </Link>
-          )} */}
+          )} 
         </div>
         {/* for mobile */}
         <div className="lg:hidden cursor-pointer flex">
